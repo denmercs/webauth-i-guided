f005140e40b4b74ac4bd8b7fac61913d03d33381
denmercs <denmercs@gmail.com>
2019-09-16 16:58:07 -0500

    answers

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
 
-const db = require('./database/dbConfig.js');
-const Users = require('./users/users-model.js');
+const Users = require("./users/users-model.js");
 
 const server = express();
 
@@ -11,12 +11,14 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
-server.post('/api/register', (req, res) => {
+server.post("/api/register", (req, res) => {
   let user = req.body;
+
+  user.password = bcrypt.hashSync(user.password, 10);
 
   Users.add(user)
     .then(saved => {
@@ -27,16 +29,19 @@ server.post('/api/register', (req, res) => {
     });
 });
 
-server.post('/api/login', (req, res) => {
+server.post("/api/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      // console.log("db password", user.password);
+      // console.log("login password", password);
+
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
@@ -44,13 +49,38 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+// this would be protected
+server.get("/api/users", restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function restricted(req, res, next) {
+  // the password should not belong in the headers
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "invalid credentials" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "unexpected error" });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password"
+    });
+  }
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
